9ec2144618714a1839c49d4e9cf9fb5798a5bbfd
Dydy003 <dcaetano8@icloud.com>
2024-06-20 11:22:43 +0200

    Initial Commit

---
+++ Dogs/DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogsApp.swift
+//  Dogs
+//
+//  Created by Dylan Caetano on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
